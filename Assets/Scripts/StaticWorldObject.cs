d9f098620f66129e8293d4ca8c165af3f47ba2ce
Bert Van Quaethem <bert.van.quaethem@student.howest.be>
2025-01-05 15:04:26 +0100

    Toggle shadows for objects

--- Assets/Scripts/StaticWorldObject.cs
+++ Assets/Scripts/StaticWorldObject.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class StaticWorldObject : MonoBehaviour
 {
     [SerializeField] private GameObject _objectFOV;
+    private Renderer _renderer;
 
     private void Awake()
     {
@@ -14,10 +15,25 @@ public class StaticWorldObject : MonoBehaviour
         {
             Debug.LogError("No object FOV found");
         }
+
+        _renderer = GetComponentInChildren<Renderer>();
+        if(!_renderer)
+        {
+            Debug.LogError("No renderer found");
+        }
     }
 
     public void ToggleObjectFOV(bool toggle)
     {
         _objectFOV.SetActive(toggle);
+
+        if(toggle)
+        {
+            _renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        }
+        else
+        {
+            _renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        } 
     }
 }
